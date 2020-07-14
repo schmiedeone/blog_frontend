f48447e212f0d0460bc381f130a9b7a8018fab36
Batch <batch@schmiede.one>
2020-07-14 16:42:55 +0200

    Add a favicon and a logo as the home button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,40 +7,46 @@ import Query from "./Query";
 import CATEGORIES_QUERY from "../queries/category/categories";
 import universalStyles from "../utils/universalStyles";
 
+import logo from "../img/schmiedeOneLogo.svg";
+
 const useStyles = makeStyles((theme) => ({
   navBar: {
     width: "80%",
-    margin: "auto",
+    // margin: "auto",
     height: "30px",
-    padding: "30px 0 30px 0",
+    margin: "30px 0 30px 0",
   },
 
   navMenuList: {
-    padding: "0",
+    // padding: "0",
     listStyleType: "none",
   },
 
   navMenuItem: {
     textDecoration: "none",
     display: "block",
-    float:"left",
+    float: "left",
   },
-  
+
   navHomeButton: {
-    float:"left",
-    fontWeight: "600",
-    fontSize: "1.5rem",
+    float: "left",
+    // fontWeight: "600",
+    // fontSize: "1.5rem",
   },
-  
+
+  navLogo: {
+    height: "100%",
+  },
+
   navCategoryButton: {
-    textTransform:"uppercase",
-    float:"right",
+    textTransform: "uppercase",
+    float: "right",
     paddingLeft: "16px",
+    display: "flex",
   },
-  
 }));
 
-const Nav = ({ name }) => {
+const Nav = () => {
   const classes = useStyles();
   const universalClasses = universalStyles();
   return (
@@ -51,12 +57,17 @@ const Nav = ({ name }) => {
             <div className={classes.navBar}>
               <ul className={classes.navMenuList}>
                 <li key={"homeButton"} className={classes.navHomeButton}>
-                  <Link to="/" className={classes.navMenuItem}>{name}</Link>
+                  <Link to="/" className={classes.navMenuItem}>
+                    <img src={logo} alt={"Schmiede.ONE Logo"} className={classes.navLogo}/>
+                  </Link>
                 </li>
                 {categories.map((category, i) => {
                   return (
                     <li key={category.id} className={classes.navCategoryButton}>
-                      <Link to={`/category/${category.id}`} className={classes.navMenuItem}>
+                      <Link
+                        to={`/category/${category.id}`}
+                        className={classes.navMenuItem}
+                      >
                         {category.name}
                       </Link>
                     </li>
